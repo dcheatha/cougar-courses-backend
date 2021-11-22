797d6bb87c8e1c8019d5cc125fe18c69315b3755
D. Cheatham <7143383+dcheatha@users.noreply.github.com>
2021-11-22 13:02:02 -0800

    Export Columns for ORM things

--- src/model/db/mod.rs
+++ src/model/db/mod.rs
@@ -2,4 +2,7 @@ mod courses;
 mod grades;
 
 pub use courses::Entity as Courses;
+pub use courses::Column as CoursesColumn;
+
 pub use grades::Entity as Grades;
+pub use grades::Column as GradesColumn;
