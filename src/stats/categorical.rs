986e648b6e1e2a78a9d89b4be373b6411dd99083
D. Cheatham <7143383+dcheatha@users.noreply.github.com>
2021-12-28 18:59:22 -0800

    Add binning to categorical stats

--- src/stats/categorical.rs
+++ src/stats/categorical.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use async_graphql as gql;
 use itertools::Itertools;
 
@@ -21,15 +23,46 @@ pub struct CategoricalStats<T: CategoricalTrait> {
 
 impl<T: CategoricalTrait> CategoricalStats<T> {
   pub fn new(data: Vec<T>) -> CategoricalStats<T> {
-    CategoricalStats {
-      data
-    }
+    CategoricalStats { data }
   }
 }
 
+#[derive(gql::SimpleObject)]
+#[graphql(concrete(name = "BinnedItemI16", params(i16)))]
+#[graphql(concrete(name = "BinnedItemI32", params(i32)))]
+#[graphql(concrete(name = "BinnedItemString", params(String)))]
+pub struct BinnedItem<T: CategoricalTrait>
+where
+  T: gql::OutputType,
+{
+  pub value: T,
+  pub frequency: usize,
+}
+
 #[gql::ComplexObject]
-impl<T: CategoricalTrait> CategoricalStats<T> {
+impl<T: CategoricalTrait> CategoricalStats<T>
+where
+  BinnedItem<T>: gql::OutputType,
+{
   async fn unique(&self) -> usize {
     self.data.iter().unique().count()
+  }
+
+  async fn bin(&self) -> Vec<BinnedItem<T>> {
+    let mut map: HashMap<T, usize> = HashMap::new();
+
+    for datum in &self.data {
+      let total = 1 + *map.get(datum).unwrap_or(&0);
+      map.insert((*datum).clone(), total);
+    }
+
+    let mut data: Vec<BinnedItem<T>> = map
+      .into_iter()
+      .map(|(value, frequency)| BinnedItem { value, frequency })
+      .collect();
+
+    data.sort_by(|a, b| b.frequency.cmp(&a.frequency));
+
+    data
   }
 }
