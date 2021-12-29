86f962e872f99f301e5004d065d62a6c49e79242
D. Cheatham <7143383+dcheatha@users.noreply.github.com>
2021-12-28 17:37:46 -0800

    Add min, max, mean, std_dev, pop_std_dev, varience, pop_varience to `DiscreteStats`

--- src/stats/discrete.rs
+++ src/stats/discrete.rs
@@ -1,4 +1,5 @@
 use async_graphql as gql;
+use statrs::statistics::{Data, Distribution, OrderStatistics, Statistics};
 
 pub struct DiscreteStats {
   data: Vec<f64>,
@@ -14,5 +15,33 @@ impl DiscreteStats {
 impl DiscreteStats {
   async fn frequency(&self) -> usize {
     self.data.len()
+  }
+
+  async fn min(&self) -> f64 {
+    Statistics::min(self.data.iter())
+  }
+
+  async fn max(&self) -> f64 {
+    Statistics::max(self.data.iter())
+  }
+
+  async fn mean(&self) -> f64 {
+    Statistics::mean(self.data.iter())
+  }
+
+  async fn std_dev(&self) -> f64 {
+    Statistics::std_dev(self.data.iter())
+  }
+
+  async fn population_std_dev(&self) -> f64 {
+    Statistics::population_std_dev(self.data.iter())
+  }
+
+  async fn variance(&self) -> f64 {
+    Statistics::variance(self.data.iter())
+  }
+
+  async fn population_variance(&self) -> f64 {
+    Statistics::population_variance(self.data.iter())
   }
 }
